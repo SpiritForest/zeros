8e7856fdde3981c151448ffd42c9bd2d1b2d3d49
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-02-24 12:46:59 +0300

    1
    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount(number){
+
+var n = 5, counter = 0;
+while (n < number){
+        counter += Math.floor(number/n);
+        n *= 5;
+}
+    return  counter;
 }
